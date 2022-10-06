5b292365aca293cff5e75a7356cc6828c8128d17
Eric Macedo Cabral <youericmacedo26@gmail.com>
2022-10-06 14:46:49 -0300

    feat: adds Sentence Bert as a document embedding strategy

---
+++ models/document_embedding/sbert.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass, field
+from multiprocessing import Process, Queue
+from typing import Iterable, List
+import pickle
+
+
+@dataclass
+class SBert:
+    model_name: str
+    embeddings: list = field(default_factory=list)
+
+    def __init__(self, model_name: str = "allenai-specter"):
+        self.model_name = model_name
+        self.embeddings = []
+
+    def _train(self, queue: Queue, corpus: List[str]):
+        from sentence_transformers import SentenceTransformer
+
+        transformer = SentenceTransformer(self.model_name)
+        embeddings = transformer.encode(corpus).tolist()
+
+        del transformer
+        SBert.clear_memory()
+
+        queue.put(embeddings)
+
+    def train(self, corpus: List[str]) -> list:
+        queue = Queue()
+        p = Process(
+            target=self._train,
+            kwargs={"queue": queue, "corpus": corpus})
+        p.start()
+        self.embeddings = queue.get()
+        p.join()
+
+        return self.embeddings
+
+    def predict(self, data: Iterable[str]) -> List[List[float]]:
+        queue = Queue()
+        p = Process(
+            target=self._train,
+            kwargs={"queue": queue, "corpus": data})
+        p.start()
+        embeddings = queue.get()
+        p.join()
+
+        return embeddings
+
+    @classmethod
+    def load(cls, path: str):
+        return pickle.load(open(path, "rb"))
+
+    @classmethod
+    def clear_memory(cls):
+        from gc import collect
+        from torch.cuda import empty_cache, ipc_collect
+
+        collect()
+        ipc_collect()
+        empty_cache()
+
+    def save(self, path: str):
+        with open(path, "wb") as pkl_file:
+            pickle.dump(
+                obj=self,
+                file=pkl_file,
+                protocol=pickle.DEFAULT_PROTOCOL,
+                fix_imports=True)
