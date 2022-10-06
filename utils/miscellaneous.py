cce06274f445a7cd68e2e6e3f6838041e002b4c6
Eric Macedo Cabral <youericmacedo26@gmail.com>
2022-10-06 14:32:12 -0300

    feat: items count dictionary function

--- utils/miscellaneous.py
+++ utils/miscellaneous.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-from typing import Callable, Iterable
+from typing import Any, Callable, Dict, Iterable, Literal
 
 
 def defaut_json_serializer(obj: object) -> object:
@@ -23,3 +23,18 @@ def apply(s: str, *args: Callable) -> str:
     for fn in args:
         s = fn(s)
     return s
+
+
+def count_dictionary(it: Iterable[Any],
+                     sort_by: Literal["key", "value"] = "value",
+                     reversed: bool = False,
+                     n: int = None) -> Dict[str, int]:
+    d = {}
+    for i in it:
+        for j in i:
+            d[j] = d.get(j, 0) + 1
+    return {
+        k: v for k, v in sorted(
+            d.items(),
+            key=lambda item: item[0 if sort_by == "key" else 1],
+            reverse=reversed)[:n]}
