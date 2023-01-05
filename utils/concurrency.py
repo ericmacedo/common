171f378472ad1f2743b6f4de9a8386520436c476
Eric Macedo Cabral <youericmacedo26@gmail.com>
2023-01-05 13:08:00 -0300

    feat: adds n_jobs to batch_processing

--- utils/concurrency.py
+++ utils/concurrency.py
@@ -1,16 +1,19 @@
 from typing import Callable
 
-from joblib import Parallel, delayed
+from joblib import Parallel, cpu_count, delayed
 from threading import Thread
 from typing import Iterable
 
+
 def batch_processing(fn: Callable, data: list, **kwargs) -> list:
-    return Parallel(n_jobs=-1, backend="multiprocessing")(
+    n_jobs = kwargs.get("n_jobs", cpu_count() - 1)
+    return Parallel(n_jobs=n_jobs, backend="multiprocessing")(
         delayed(fn)(data=i, **kwargs) for i in data)
 
+
 def threading(fn: Callable, wait: bool = False, args: Iterable = ()) -> None:
-	t = Thread(target=fn, args=args)
-	t.start()
-	
-	if wait:
-		t.join()
+    t = Thread(target=fn, args=args)
+    t.start()
+
+    if wait:
+        t.join()
